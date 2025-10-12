9c6b6e3a77ee93f1073a32f9a728d8e99e1d0fab
Vitalii <karpvits@gmail.com>
2025-10-12 20:32:26 +0200

    add mat-table

--- src/app/components/person-dashboard/person-dashboard.ts
+++ src/app/components/person-dashboard/person-dashboard.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { PersonService } from '../../services/person/person.service';
 import { Person } from '../../models/person.model';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-person-dashboard',
   templateUrl: './person-dashboard.html',
   styleUrls: ['./person-dashboard.scss'],
   providers: [PersonService],
-  imports: [AsyncPipe, JsonPipe]
+  imports: [AsyncPipe, MatTableModule],
 })
 export class PersonDashboard implements OnInit {
   persons$: Observable<Person[]>;
+  displayedColumns = ['name', 'piiTypes', 'dataSources'];
 
   constructor(private personService: PersonService) {
     this.persons$ = this.personService.persons$;
