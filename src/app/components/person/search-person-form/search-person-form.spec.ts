c08c2e0bcb565203ebb7bb7a4e21af15ff086fd1
Vitalii <karpvits@gmail.com>
2025-10-15 20:31:57 +0200

    unit tests for SearchPersonForm

--- src/app/components/person/search-person-form/search-person-form.spec.ts
+++ src/app/components/person/search-person-form/search-person-form.spec.ts
@@ -1,13 +1,23 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import { SearchPersonForm } from './search-person-form';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
+import { selectPiiTypeOpts } from '../../../store/persons/persons.selectors';
+import { updateFilter } from '../../../store/filter/filter.reducer';
 
 describe('SearchPersonForm', () => {
   let component: SearchPersonForm;
   let fixture: ComponentFixture<SearchPersonForm>;
+  let storeSpy: jasmine.SpyObj<any>;
 
   beforeEach(waitForAsync(() => {
+    storeSpy = jasmine.createSpyObj('Store', ['select', 'dispatch']);
+    storeSpy.select.and.returnValue(of([]));
     TestBed.configureTestingModule({
-      declarations: [ SearchPersonForm ]
+      imports: [SearchPersonForm],
+      providers: [
+        { provide: Store, useValue: storeSpy },
+      ],
     })
     .compileComponents();
   }));
@@ -21,4 +31,30 @@ describe('SearchPersonForm', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have form controls name and pii', () => {
+    expect(component.form.contains('name')).toBeTrue();
+    expect(component.form.contains('pii')).toBeTrue();
+  });
+
+  it('should select piiTypesOpts$ from store', () => {
+    expect(storeSpy.select).toHaveBeenCalledWith(selectPiiTypeOpts);
+  });
+
+  it('should dispatch updateFilter when filterSignal changes', fakeAsync(() => {
+    storeSpy.dispatch.calls.reset();
+    component.form.controls.pii.setValue(['email']);
+    fixture.detectChanges();
+    expect(storeSpy.dispatch).toHaveBeenCalledWith(updateFilter({ filter: { name: null, piiTypes: [ 'email' ] } }));
+    
+
+    storeSpy.dispatch.calls.reset();
+    component.form.controls.name.setValue('test');
+    fixture.detectChanges();
+
+    // Wait for effect to trigger
+    tick(600);
+    fixture.detectChanges();
+    expect(storeSpy.dispatch).toHaveBeenCalledWith(updateFilter({ filter: { name: 'test', piiTypes: [ 'email' ] } }));
+  }));
 });
